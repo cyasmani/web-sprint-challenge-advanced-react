9159ccaf05650bf089c2ddebbd0e18a7fa47bd7d
cyasmani <yasmanicastaneda@yahoo.com>
2020-09-11 19:15:06 -0700

    Passed both tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,52 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render , fireEvent} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { act } from "react-dom/test-utils";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", async () => {
+    const { getByPlaceholderText, getByLabelText , getByText} = render(<CheckoutForm/>);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async() => {
+
+    const { getByPlaceholderText, getByLabelText , getByText, getByRole} = render(<CheckoutForm/>);
+
+    const firstName = getByLabelText(/First Name:/i)
+    const lastName = getByLabelText(/Last Name:/i)
+    const address = getByLabelText(/Address:/i)
+    const city = getByLabelText(/City:/i)
+    const state = getByLabelText(/State:/i)
+    const zip = getByLabelText(/Zip:/i)
+
+    fireEvent.change(firstName, {
+        target: {value :"Big Gucci Sosa"}
+    })
+    
+    fireEvent.change(lastName, {
+        target: {value: "3 Hunnid Hunnid"}
+    })
+
+    fireEvent.change(address, {
+        target:{value: "fresh prince of bel-air"}
+    })
+
+    fireEvent.change(city, {
+        target: {value: "Wakanda"}
+    })
+
+    fireEvent.change(state, {
+        target: {value: "Pangea"}
+    })
+
+    fireEvent.change(zip, {
+        target: {value: "89142"}
+    })
+
+    await act(async() => {
+        fireEvent.click(getByRole('button'))
+    })
+
+
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -3,15 +3,15 @@ import {useState} from "react";
 
 
 const useInput = initialValue => {
-    //useInput will pretty much be the same as useState
+
       const [value, setValue] = useState(initialValue);
-      //setup state, initial value will hold value of username and password
+
       const handleChanges = updatedValue => {
         setValue(updatedValue);
-        //whenever a change occurs it will update state
+
       };
       return [value, setValue, handleChanges];
-      //this allows me to have state and handleChanges in the same hook. Cleaner code
+
   };
 
   export default useInput
