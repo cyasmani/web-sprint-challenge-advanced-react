7d0e057c4a21932027e74956bd787981993eeaf8
cyasmani <yasmanicastaneda@yahoo.com>
2020-09-11 19:48:30 -0700

    edited a few things

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -3,17 +3,17 @@ import {useState} from "react";
 const useLocalStorage = (key, initialValue) => {
 
     const [storedValue, setStoredValue] = useState(() => {
-        //set up state
+
         const item = window.localStorage.getItem(key)
-        //gets the key from localstorage
+
        console.log(item)
 
         return item ? JSON.parse(item) : initialValue;
-        //QUESTION: Why is there nothing in LocalStorage? This still goes dark mode without the code here
+
     })
 
     const setValue = value => {
-        //setter function
+
         setStoredValue(value);
         window.localStorage.setItem(key, JSON.stringify(value))
     }
